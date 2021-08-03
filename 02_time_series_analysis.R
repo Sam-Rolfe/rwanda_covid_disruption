0e68178fde4a3c975e739b10ff6f074c80cbeaea
srolfe <srolfe@uw.edu>
2021-08-03 14:28:23 -0700

    Time series analysis v1 complete

--- 02_time_series_analysis.R
+++ 02_time_series_analysis.R
@@ -15,22 +15,228 @@ library(ggplot2)
 
 #----GLOBAL VARIABLES--------------------------------------------------------------------------------------------------
 # File Paths
-root      <- "/ihme/homes/srolfe/projects/rwanda_covid_disruption/"
-data_path <- file.path(root, "01_data", "01_raw_data", "data_requested_HMIS_27032021.csv")
+project_root               <- "/ihme/homes/srolfe/projects/rwanda_covid_disruption/"
+data_root                  <- file.path(project_root, "01_data", "02_prepped_data")
+diagnostic_root            <- file.path(project_root, "03_diagnostics")
+
+prepped_data_path          <- file.path(data_root, "prepped_data.csv")
+disruption_ratio_data_path <- file.path(data_root, "disruption.csv")
 
 #----FUNCTIONS---------------------------------------------------------------------------------------------------------
 
-#--- 2a. PLOT: DOSES ADMINISTERED -------------------------------------------------------------------------------------
+#--- 1. GET DATA: DOSES ADMINISTERED --------------------------------------------------------------------------------
 # Get prepped data
-data <- fread(file.path(root, "01_data", "02_prepped_data", "full_data_with_disruption.csv"))
+data            <- fread(prepped_data_path)
+disruption_data <- fread(disruption_ratio_data_path, 
+                         select = c("district", "month", "year", 
+                                    "mr1_disruption_ratio", "mr1_cumulative_disruption_ratio", 
+                                    "dpt3_disruption_ratio", "dpt3_cumulative_disruption_ratio"))
 
-#--- 2b. PLOT: DISRUPTION RATIO ---------------------------------------------------------------------------------------
+
+# Add date
+data[, date := as.Date(paste(year, month, "01", sep = "-"), format = "%Y-%m-%d")]
+disruption_data[, date := as.Date(paste(year, month, "01", sep = "-"), format = "%Y-%m-%d")]
+
+# Get district for ease of plotting
+districts   <- sort(unique(data$district))
+n_districts <- length(districts)
+
+# Identify districts per panel
+districts_per_panel <- 6
+n_panels            <- n_districts / districts_per_panel
+
+# Get values used in plotting (y-max)
+y_max_mr1  <- max(data$mr1)
+y_max_dpt3 <- max(data$dpt3)
+
+y_max_disruption  <- max(c(disruption_data$mr1_cumulative_disruption_ratio, 
+                           disruption_data$dpt3_cumulative_disruption_ratio, 
+                           disruption_data$mr1_disruption_ratio, 
+                           disruption_data$dpt3_disruption_ratio))
+y_min_disruption  <- min(c(disruption_data$mr1_cumulative_disruption_ratio, 
+                           disruption_data$dpt3_cumulative_disruption_ratio, 
+                           disruption_data$mr1_disruption_ratio, 
+                           disruption_data$dpt3_disruption_ratio))
+#--- 2. PLOT: DOSES ADMINISTERED ------------------------------------------------------------------------------------
+
+# Open PDF for plotting
+pdf(file = file.path(diagnostic_root, "time_series_analysis.pdf"))
+
+# Plot MR1 doses administered for each district in n_panels
+for(i in seq.int(n_panels)) {
+  
+  # Get districts of interest
+  index_district_start  <- ((i - 1) * 6) + 1
+  index_district_end    <- i * 6 
+  districts_of_interest <- districts[index_district_start:index_district_end] 
+  
+  gg <- ggplot(data[district %in% districts_of_interest], mapping = aes(x = date, y = mr1)) +
+    geom_point() +
+    geom_line() +
+    ylim(c(0, y_max_mr1)) +
+    ylab("Doses Administered") +
+    ggtitle("MR1: Doses Administered") +
+    facet_wrap(~ district) +
+    theme_bw() +
+    theme(axis.title.x = element_blank()) 
+
+  plot(gg)
+  
+}
+
+
+# Plot DPT3 doses administered for each district in n_panels
+for(i in seq.int(n_panels)) {
+  
+  # Get districts of interest
+  index_district_start  <- ((i - 1) * 6) + 1
+  index_district_end    <- i * 6 
+  districts_of_interest <- districts[index_district_start:index_district_end] 
+  
+  gg <- ggplot(data[district %in% districts_of_interest], mapping = aes(x = date, y = dpt3)) +
+    geom_point() +
+    geom_line() +
+    ylim(c(0, y_max_dpt3)) +
+    ylab("Doses Administered") +
+    ggtitle("DPT3: Doses Administered") +
+    facet_wrap(~ district) +
+    theme_bw() +
+    theme(axis.title.x = element_blank())
+  
+  plot(gg)
+}
 
 
 
+#--- 3. PLOT: DISRUPTION RATIO - NORMAL -----------------------------------------------------------------------------
+
+# Plot MR1 disruption ratio for each district in n_panels
+for(i in seq.int(n_panels)) {
+  
+  # Get districts of interest
+  index_district_start  <- ((i - 1) * 6) + 1
+  index_district_end    <- i * 6 
+  districts_of_interest <- districts[index_district_start:index_district_end] 
+  
+  gg <- ggplot(disruption_data[district %in% districts_of_interest], mapping = aes(x = date, y = mr1_disruption_ratio)) +
+    geom_point() +
+    geom_line() +
+    geom_hline(yintercept = 1, 
+               linetype = "dashed", 
+               color = "red") +
+    ylim(c(y_min_disruption, 
+           y_max_disruption)) +
+    ylab("Disruption Ratio") +
+    scale_x_date(date_labels = "%b", 
+                 date_breaks  ="1 month") +
+    ggtitle("MR1: Disruption Ratio") +
+    facet_wrap(~ district) +
+    theme_bw() +
+    theme(axis.title.x = element_blank(), 
+          axis.text.x = element_text(angle = 45, vjust = 0.5, hjust=1))
+  
+  plot(gg)
+  
+}
+
+
+# Plot DPT3 disruption ratio for each district in n_panels
+for(i in seq.int(n_panels)) {
+  
+  # Get districts of interest
+  index_district_start  <- ((i - 1) * 6) + 1
+  index_district_end    <- i * 6 
+  districts_of_interest <- districts[index_district_start:index_district_end] 
+  
+  gg <- ggplot(disruption_data[district %in% districts_of_interest], mapping = aes(x = date, y = dpt3_disruption_ratio)) +
+    geom_point() +
+    geom_line() +
+    geom_hline(yintercept = 1, 
+               linetype = "dashed", 
+               color = "red") +
+    ylim(c(y_min_disruption, 
+           y_max_disruption)) +    ylab("Disruption Ratio") +
+    scale_x_date(date_labels = "%b", 
+                 date_breaks  ="1 month") +
+    ggtitle("DPT3: Disruption Ratio") +
+    facet_wrap(~ district) +
+    theme_bw() +
+    theme(axis.title.x = element_blank(), 
+          axis.text.x = element_text(angle = 45, vjust = 0.5, hjust=1))
+  
+  plot(gg)
+  
+}
+
+
+
+
+
+#--- 4. PLOT: DISRUPTION RATIO - CUMULATIVE --------------------------------------------------------------------
+
+
+# Plot MR1 cumulative disruption ratio for each district in n_panels
+for(i in seq.int(n_panels)) {
+  
+  # Get districts of interest
+  index_district_start  <- ((i - 1) * 6) + 1
+  index_district_end    <- i * 6 
+  districts_of_interest <- districts[index_district_start:index_district_end] 
+  
+  gg <- ggplot(disruption_data[district %in% districts_of_interest], mapping = aes(x = date, y = mr1_cumulative_disruption_ratio)) +
+    geom_point() +
+    geom_line() +
+    geom_hline(yintercept = 1, 
+               linetype = "dashed", 
+               color = "red") +
+    ylim(c(y_min_disruption, 
+           y_max_disruption)) +
+    ylab("Disruption Ratio (Cumulative)") +
+    scale_x_date(date_labels = "%b", 
+                 date_breaks  ="1 month") +
+    ggtitle("MR1: Disruption Ratio (Cumulative)") +
+    facet_wrap(~ district) +
+    theme_bw() +
+    theme(axis.title.x = element_blank(), 
+          axis.text.x = element_text(angle = 45, vjust = 0.5, hjust=1))
+  
+  plot(gg)
+  
+}
+
+
+# Plot DPT3 disruption ratio for each district in n_panels
+for(i in seq.int(n_panels)) {
+  
+  # Get districts of interest
+  index_district_start  <- ((i - 1) * 6) + 1
+  index_district_end    <- i * 6 
+  districts_of_interest <- districts[index_district_start:index_district_end] 
+  
+  gg <- ggplot(disruption_data[district %in% districts_of_interest], mapping = aes(x = date, y = dpt3_cumulative_disruption_ratio)) +
+    geom_point() +
+    geom_line() +
+    geom_hline(yintercept = 1, 
+               linetype = "dashed", 
+               color = "red") +
+    ylim(c(y_min_disruption, 
+           y_max_disruption)) +
+    ylab("Disruption Ratio (Cumulative)") +
+    scale_x_date(date_labels = "%b", 
+                 date_breaks  ="1 month") +
+    ggtitle("DPT3: Disruption Ratio (Cumulative)") +
+    facet_wrap(~ district) +
+    theme_bw() +
+    theme(axis.title.x = element_blank(), 
+          axis.text.x = element_text(angle = 45, vjust = 0.5, hjust=1))
+  
+  plot(gg)
+}
+
+# Close PDF
+dev.off()
 
 
 #**********************************************************************************************************************
 
 
-#**********************************************************************************************************************
